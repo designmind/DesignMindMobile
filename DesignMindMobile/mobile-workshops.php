7c8514b158225b8f240700a9d84a65241e664203
Michael Silva <michaelsilva@me.com>
2016-06-16 15:44:58 -0700

    Add basic mailto script.
    Convert HTML to PHP.

--- DesignMindMobile/mobile-workshops.php
+++ DesignMindMobile/mobile-workshops.php
@@ -1,17 +1,51 @@
-﻿<!DOCTYPE html>
+<?php
+  if (isset($_POST["submit"])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'DesignMind Mobile';
+    $to = 'mobile@designmind.com';
+    $subject = 'Message from Contact';
+
+    $body ="From: $name\n E-Mail: $email\n Message:\n $message";
+    // Check if name has been entered
+    if (!$_POST['name']) {
+      $errName = 'Please enter your name';
+    }
+
+    // Check if email has been entered and is valid
+    if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+      $errEmail = 'Please enter a valid email address';
+    }
+
+    //Check if message has been entered
+    if (!$_POST['message']) {
+      $errMessage = 'Please enter your message';
+    }
+
+    // If there are no errors, send the email
+    if (!$errName && !$errEmail && !$errMessage) {
+      if (mail ($to, $subject, $body, $from)) {
+        $result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+      } else {
+        $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later.</div>';
+      }
+    }
+  }
+?>
+<!DOCTYPE html>
 <html>
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <meta name="description" content="DesignMind Mobile is a San Francisco based mobile development and strategy company serving complete application development including Big Data, Business Intelligence and Data Analytics." />
     <meta name="keywords" content="san francisco iPhone development, iPhone developers, iPhone apps, iOS development, iOS developers, iPad development, iPad developers, iPad apps, mobile app design, mobile app development,
           mobile app developers, mobile application development, mobile application developer, product consultants, product design, software consultants, android developers, andriod, swift, java, firebase, azure, aws, aws mobile, azure mobile apps," />
-    <title>UpModel Case Study | San Francisco Mobile Development Agency and Data Experts</title>
+    <title>Sawyer Case Study | San Francisco Mobile Development Agency and Data Experts</title>
     <meta charset="utf-8" />
     <meta name="robots" content="noodp,noydir">
 
-    <link rel="canonical" href="http://designmindmobile.com/UpModel_Subpage.html" />
+    <link rel="canonical" href="http://designmindmobile.com/mobile-workshop.php" />
     <link rel="publisher" href="https://plus.google.com/103267323369198800190/about" />
     <link rel="Shortcut Icon" href="http://c8zyre51kh-flywheel.netdna-ssl.com/wp-content/themes/designmindRWD/images/favicon.ico" type="image/x-icon" />
     <meta property="og:locale" content="en_US" />
@@ -25,7 +59,7 @@
     <meta property="og:site_name" content="designmindmobile">
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:description" content="DesignMind Mobile is a San Francisco based mobile development and strategy company serving complete application development including Big Data, Business Intelligence and Data Analytics." />
-    <meta name="twitter:title" content="UpModel Case Study San Francisco App Development | Custom Mobile App Developers" />
+    <meta name="twitter:title" content="Sawyer Case Study San Francisco App Development | Custom Mobile App Developers" />
     <meta name="twitter:site" content="@designmindmobile" />
     <meta name="twitter:creator" content="@k3street" />
     <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
@@ -35,7 +69,7 @@
     <link href="css/styles.css" rel="stylesheet">
   </head>
 
-  <body id="top" data-spy="scroll" data-target="#navbar">
+  <body id="top" data-spy="scroll" data-target="#navbar" class="page-workshops">
     <!-- Navigation -->
     <nav class="navbar navbar-default navbar-fixed-top">
       <div class="container">
@@ -46,20 +80,20 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="/index.html#top">
+          <a class="navbar-brand" href="/index.php#top">
             <img src="img/logo.jpg" alt="DesignMind Mobile Enterprise" description="Logo for DesignMind Mobile." />
           </a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav pull-right">
-            <li><a data-toggle="collapse" data-target=".navbar-collapse" href="/index.html#toolbox">Toolbox</a></li>
-            <li><a data-toggle="collapse" data-target=".navbar-collapse" href="/index.html#workshops">Process</a></li>
-            <li><a data-toggle="collapse" data-target=".navbar-collapse" href="/leadership.html">Leadership</a></li>
+            <li><a data-toggle="collapse" data-target=".navbar-collapse" href="/index.php#toolbox">Toolbox</a></li>
+            <li><a data-toggle="collapse" data-target=".navbar-collapse" href="/index.php#workshops">Process</a></li>
+            <li><a data-toggle="collapse" data-target=".navbar-collapse" href="/leadership.php">Leadership</a></li>
             <li class="dropdown">
               <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Accelerators <span class="caret"></span></a>
               <ul class="dropdown-menu">
-                <li><a href="/sawyer.html">Sawyer Brewing</a></li>
-                <li><a href="/upmodel.html">UpModel</a></li>
+                <li><a href="/sawyer.php">Sawyer Brewing</a></li>
+                <li><a href="/upmodel.php">UpModel</a></li>
               </ul>
             </li>
             <li><a data-toggle="collapse" data-target=".navbar-collapse" href="#contact">Contact</a></li>
@@ -68,59 +102,29 @@
       </div>
     </nav>
 
-    <section class="sawyer">
+    <section class="workshops" id="workshops">
       <div class="container">
-        <div class="row">
-          <div class="col-sm-6">
-            <img src="img/upmodel.jpg">
-          </div>
-          <div class="col-sm-6">
-            <div class="row">
-              <div class="col-sm-12">
-                <div class="title">
-                  <h1>Case Study:<br/>UpModel</h1>
-                  <p>UpModel Renovations upgrades homes throughout the San Francisco Bay Area.  They needed an app for their employees to easily capture and send information to headquarters about clients and sales opportunities.</p>
-                </div>
-              </div>
-            </div>
-            <div class="row point">
-              <div class="col-xs-4">
-                <img class="img-rounded icon" src="img/Sawyer_Subpage_challenge.png" alt="Challenge icon">
-              </div>
-              <div class="col-xs-8">
-                <h2>Our Challenge</h2>
-                <p>UpModel workers take measurements, and photos for each client, and plan the new project. With the app, they can send this data efficiently to headquarters.  Renovation experts receive the information, including designs and supplies needed.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-sm-6">
-            <div class="row point">
-              <div class="col-xs-4">
-                <img class="img-rounded icon" src="img/Sawyer_Subpage_solution.png" alt="Solution icon">
-              </div>
-              <div class="col-xs-8">
-                <h2>Our Solution</h2>
-                <p>Using our Mobile Accelerators, we created an app that provides up-to-the-minute data exchanges between field workers and headquarters. The app keeps track of jobs, supplies, revenues and length of projects. Customer estimates are faster and more accurate, and the sales cycle was shortened.</p>
-              </div>
-            </div>
-            <div class="row point">
-              <div class="col-xs-4">
-                <img class="img-rounded icon" src="img/Sawyer_Subpage_result.png" alt="Solution icon">
-              </div>
-              <div class="col-xs-8">
-                <h2>The Results</h2>
-                <p>Dashboards can give each regional manager a quick overview of his team’s projects and revenues.  Better communication yields faster decisions and better results.</p>
-              </div>
-            </div>
-            <a class="btn btn-default pull-right" href="#contact" role="button">Get Started <i class="fa fa-chevron-right" aria-hidden="true"></i></a>
-          </div>
-          <div class="col-sm-6 phone hidden-xs">
-            <img src="img/upmodel_android.jpg">
-          </div>
-        </div>
-      </div>
+        <div class="col-sm-6 col-sm-offset-6 content">
+          <h1>Mobile Workshops</h1>
+          <p>Schedule a 2-3 day workshop where we will gather requirements, define constraints, discuss objectives and develop the mobile opportunity. The first step is for our professionals to understand your business and the challenges you face.</p>
+          <p>We’ll co-develop a plan and determine deliverables in only a few days.</p>
+          <ul>
+            <li>
+              <h3>Day 1</h3>
+              <p>Stakeholder strategy sessions at your office, requirements gathering, and needs identification.</p>
+            </li>
+            <li>
+              <h3>Day 2</h3>
+              <p>Small group design break-outs, security and technical constraints, and rapid prototyping.</p>
+            </li>
+            <li>
+              <h3>Day 3</h3>
+              <p>Our team creates a timeline, minimum viable product, and demos the user experience.  Wireframing begins.</p>
+            </li>
+          </ul>
+          <p><a class="btn btn-default" href="#contact" role="button">Get started <i class="fa fa-chevron-right" aria-hidden="true"></i></a></p>
+       </div>
+     </div>
     </section>
     <section class="contact" id="contact">
       <div class="container">
@@ -135,23 +139,29 @@
           <a href="https://twitter.com/DesignMindData" target="_blank"><i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i></a>
           <a href="https://www.linkedin.com/company/designmind?trk=top_nav_home" target="_blank"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
         </div>
-        <form role="form" action="" method="post" class="col-sm-8" >
+        <form class="col-sm-8" role="form" method="post" action="index.php">
           <div class="row">
-            <div class="form-group col-sm-6">
-              <label for="InputName">Your Name</label>
-              <input type="text" class="form-control" name="InputName" id="InputName" placeholder="Enter Name" required>
-            </div>
-            <div class="form-group col-sm-6">
-              <label for="InputEmail">Your Email</label>
-              <input type="email" class="form-control" id="InputEmail" name="InputEmail" placeholder="Enter Email" required>
-            </div>
             <div class="form-group col-xs-12">
-              <label for="InputMessage">Message</label>
-              <textarea name="InputMessage" id="InputMessage" class="form-control" rows="5" required></textarea>
+              <?php echo $result; ?>
             </div>
+  					<div class="form-group col-sm-6">
+  						<label for="name">Name</label>
+  						<input type="text" class="form-control" id="name" name="name" placeholder="Enter Name" value="<?php echo htmlspecialchars($_POST['name']); ?>">
+  						<?php echo "<p class='text-danger'>$errName</p>";?>
+  					</div>
+  					<div class="form-group col-sm-6">
+  						<label for="email">Email</label>
+  						<input type="email" class="form-control" id="email" name="email" placeholder="Enter Email" value="<?php echo htmlspecialchars($_POST['email']); ?>">
+  						<?php echo "<p class='text-danger'>$errEmail</p>";?>
+  					</div>
+  					<div class="form-group col-xs-12">
+  						<label for="message">Message</label>
+  						<textarea class="form-control" rows="5" name="message"><?php echo htmlspecialchars($_POST['message']);?></textarea>
+  						<?php echo "<p class='text-danger'>$errMessage</p>";?>
+  					</div>
+            <input type="submit" name="submit" id="submit" value="Submit" class="btn btn-info pull-right">
           </div>
-          <input type="submit" name="submit" id="submit" value="Submit" class="btn btn-info pull-right">
-        </form>
+				</form>
       </div>
     </section>
     <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
--- DesignMindMobile/upmodel.php
+++ DesignMindMobile/upmodel.php
@@ -1,16 +1,52 @@
-﻿<!DOCTYPE html>
+<?php
+  if (isset($_POST["submit"])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'DesignMind Mobile';
+    $to = 'mobile@designmind.com';
+    $subject = 'Message from Contact';
+
+    $body ="From: $name\n E-Mail: $email\n Message:\n $message";
+    // Check if name has been entered
+    if (!$_POST['name']) {
+      $errName = 'Please enter your name';
+    }
+
+    // Check if email has been entered and is valid
+    if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+      $errEmail = 'Please enter a valid email address';
+    }
+
+    //Check if message has been entered
+    if (!$_POST['message']) {
+      $errMessage = 'Please enter your message';
+    }
+
+    // If there are no errors, send the email
+    if (!$errName && !$errEmail && !$errMessage) {
+      if (mail ($to, $subject, $body, $from)) {
+        $result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+      } else {
+        $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later.</div>';
+      }
+    }
+  }
+?>
+<!DOCTYPE html>
 <html>
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <meta name="description" content="DesignMind Mobile is a San Francisco based mobile development and strategy company serving complete application development including Big Data, Business Intelligence and Data Analytics." />
     <meta name="keywords" content="san francisco iPhone development, iPhone developers, iPhone apps, iOS development, iOS developers, iPad development, iPad developers, iPad apps, mobile app design, mobile app development,
           mobile app developers, mobile application development, mobile application developer, product consultants, product design, software consultants, android developers, andriod, swift, java, firebase, azure, aws, aws mobile, azure mobile apps," />
-    <title>Sawyer Case Study | San Francisco Mobile Development Agency and Data Experts</title>
+    <title>UpModel Case Study | San Francisco Mobile Development Agency and Data Experts</title>
     <meta charset="utf-8" />
     <meta name="robots" content="noodp,noydir">
 
-    <link rel="canonical" href="http://designmindmobile.com/Sawyer_Subpage.html" />
+    <link rel="canonical" href="http://designmindmobile.com/upmodel.php" />
     <link rel="publisher" href="https://plus.google.com/103267323369198800190/about" />
     <link rel="Shortcut Icon" href="http://c8zyre51kh-flywheel.netdna-ssl.com/wp-content/themes/designmindRWD/images/favicon.ico" type="image/x-icon" />
     <meta property="og:locale" content="en_US" />
@@ -24,7 +60,7 @@
     <meta property="og:site_name" content="designmindmobile">
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:description" content="DesignMind Mobile is a San Francisco based mobile development and strategy company serving complete application development including Big Data, Business Intelligence and Data Analytics." />
-    <meta name="twitter:title" content="Sawyer Case Study San Francisco App Development | Custom Mobile App Developers" />
+    <meta name="twitter:title" content="UpModel Case Study San Francisco App Development | Custom Mobile App Developers" />
     <meta name="twitter:site" content="@designmindmobile" />
     <meta name="twitter:creator" content="@k3street" />
     <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
@@ -45,20 +81,20 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="/index.html#top">
+          <a class="navbar-brand" href="/index.php#top">
             <img src="img/logo.jpg" alt="DesignMind Mobile Enterprise" description="Logo for DesignMind Mobile." />
           </a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav pull-right">
-            <li><a data-toggle="collapse" data-target=".navbar-collapse" href="/index.html#toolbox">Toolbox</a></li>
-            <li><a data-toggle="collapse" data-target=".navbar-collapse" href="/index.html#workshops">Process</a></li>
-            <li><a data-toggle="collapse" data-target=".navbar-collapse" href="/leadership.html">Leadership</a></li>
+            <li><a data-toggle="collapse" data-target=".navbar-collapse" href="/index.php#toolbox">Toolbox</a></li>
+            <li><a data-toggle="collapse" data-target=".navbar-collapse" href="/index.php#workshops">Process</a></li>
+            <li><a data-toggle="collapse" data-target=".navbar-collapse" href="/leadership.php">Leadership</a></li>
             <li class="dropdown">
               <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Accelerators <span class="caret"></span></a>
               <ul class="dropdown-menu">
-                <li><a href="/sawyer.html">Sawyer Brewing</a></li>
-                <li><a href="/upmodel.html">UpModel</a></li>
+                <li><a href="/sawyer.php">Sawyer Brewing</a></li>
+                <li><a href="/upmodel.php">UpModel</a></li>
               </ul>
             </li>
             <li><a data-toggle="collapse" data-target=".navbar-collapse" href="#contact">Contact</a></li>
@@ -71,14 +107,14 @@
       <div class="container">
         <div class="row">
           <div class="col-sm-6">
-            <img src="img/sawyer.jpg">
+            <img src="img/upmodel.jpg">
           </div>
           <div class="col-sm-6">
             <div class="row">
               <div class="col-sm-12">
                 <div class="title">
-                  <h1>Case Study:<br/>Sawyer Brewing</h1>
-                  <p>Sawyer is a regional organic brewery located in Northern California. They distribute their products to 378 stores and wholesalers on the West Coast. Competition for shelf space is keen and the delivery and sales teams need to coordinate efficiently to sell more product.</p>
+                  <h1>Case Study:<br/>UpModel</h1>
+                  <p>UpModel Renovations upgrades homes throughout the San Francisco Bay Area.  They needed an app for their employees to easily capture and send information to headquarters about clients and sales opportunities.</p>
                 </div>
               </div>
             </div>
@@ -88,7 +124,7 @@
               </div>
               <div class="col-xs-8">
                 <h2>Our Challenge</h2>
-                <p>Sawyer management was concerned with their field team’s ability to service clients in a timely fashion. The clients needed faster delivery, and assurance that Sawyer would respond quickly to their inventory needs. The manual process was inefficient and prone to error. The Sawyer field workers needed to relay information to headquarters quickly and accurately.</p>
+                <p>UpModel workers take measurements, and photos for each client, and plan the new project. With the app, they can send this data efficiently to headquarters.  Renovation experts receive the information, including designs and supplies needed.</p>
               </div>
             </div>
           </div>
@@ -101,7 +137,7 @@
               </div>
               <div class="col-xs-8">
                 <h2>Our Solution</h2>
-                <p>DesignMind’s mobile team used our Mobile Accelerators to quickly design and build the app which connects to the inventory system. It also provides management with crucial business intelligence capabilities.</p>
+                <p>Using our Mobile Accelerators, we created an app that provides up-to-the-minute data exchanges between field workers and headquarters. The app keeps track of jobs, supplies, revenues and length of projects. Customer estimates are faster and more accurate, and the sales cycle was shortened.</p>
               </div>
             </div>
             <div class="row point">
@@ -110,13 +146,13 @@
               </div>
               <div class="col-xs-8">
                 <h2>The Results</h2>
-                <p>Sawyer’s sales increased 25% after the app had been in use for only two months. Because the delivery team was able to make deliveries faster with fewer mistakes, shelf space was used more effectively, and forecasting was improved.</p>
+                <p>Dashboards can give each regional manager a quick overview of his team’s projects and revenues.  Better communication yields faster decisions and better results.</p>
               </div>
             </div>
             <a class="btn btn-default pull-right" href="#contact" role="button">Get Started <i class="fa fa-chevron-right" aria-hidden="true"></i></a>
           </div>
           <div class="col-sm-6 phone hidden-xs">
-            <img src="img/sawyer_android.jpg">
+            <img src="img/upmodel_android.jpg">
           </div>
         </div>
       </div>
@@ -134,23 +170,29 @@
           <a href="https://twitter.com/DesignMindData" target="_blank"><i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i></a>
           <a href="https://www.linkedin.com/company/designmind?trk=top_nav_home" target="_blank"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
         </div>
-        <form role="form" action="" method="post" class="col-sm-8" >
+        <form class="col-sm-8" role="form" method="post" action="index.php">
           <div class="row">
-            <div class="form-group col-sm-6">
-              <label for="InputName">Your Name</label>
-              <input type="text" class="form-control" name="InputName" id="InputName" placeholder="Enter Name" required>
-            </div>
-            <div class="form-group col-sm-6">
-              <label for="InputEmail">Your Email</label>
-              <input type="email" class="form-control" id="InputEmail" name="InputEmail" placeholder="Enter Email" required>
-            </div>
             <div class="form-group col-xs-12">
-              <label for="InputMessage">Message</label>
-              <textarea name="InputMessage" id="InputMessage" class="form-control" rows="5" required></textarea>
+              <?php echo $result; ?>
             </div>
+  					<div class="form-group col-sm-6">
+  						<label for="name">Name</label>
+  						<input type="text" class="form-control" id="name" name="name" placeholder="Enter Name" value="<?php echo htmlspecialchars($_POST['name']); ?>">
+  						<?php echo "<p class='text-danger'>$errName</p>";?>
+  					</div>
+  					<div class="form-group col-sm-6">
+  						<label for="email">Email</label>
+  						<input type="email" class="form-control" id="email" name="email" placeholder="Enter Email" value="<?php echo htmlspecialchars($_POST['email']); ?>">
+  						<?php echo "<p class='text-danger'>$errEmail</p>";?>
+  					</div>
+  					<div class="form-group col-xs-12">
+  						<label for="message">Message</label>
+  						<textarea class="form-control" rows="5" name="message"><?php echo htmlspecialchars($_POST['message']);?></textarea>
+  						<?php echo "<p class='text-danger'>$errMessage</p>";?>
+  					</div>
+            <input type="submit" name="submit" id="submit" value="Submit" class="btn btn-info pull-right">
           </div>
-          <input type="submit" name="submit" id="submit" value="Submit" class="btn btn-info pull-right">
-        </form>
+				</form>
       </div>
     </section>
     <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
